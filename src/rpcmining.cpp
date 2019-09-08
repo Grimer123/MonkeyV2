11cac81dcb977e20bb1f9c0d490c24fe8d4305ce
Grimer123 <35968798+Grimer123@users.noreply.github.com>
2019-09-08 11:02:15 +0300

    fix

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -227,7 +227,7 @@ UniValue generateBlocks(boost::shared_ptr<CReserveScript> coinbaseScript, int nG
     UniValue blockHashes(UniValue::VARR); 
     while (nHeight < nHeightEnd) 
     { 
-        std::unique_ptr<CBlockTemplate> pblocktemplate(BlockAssembler(Params()).CreateNewBlock(coinbaseScript->reserveScript)); 
+        unique_ptr<CBlockTemplate> pblocktemplate(CreateNewBlockWithKey(reservekey, pwalletMain, false));
         if (!pblocktemplate.get()) 
             throw JSONRPCError(RPC_INTERNAL_ERROR, "Couldn't create new block"); 
         CBlock *pblock = &pblocktemplate->block; 
